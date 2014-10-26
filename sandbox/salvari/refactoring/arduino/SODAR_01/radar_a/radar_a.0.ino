29db65611442c8faa6c4e30bd7661dab29e8bde5
Sergio Alvariño <salvari@gmail.com>
2014-10-26 22:16:50 +0100

    Mas cambios en la parte de Arduino

---
+++ sandbox/salvari/refactoring/arduino/SODAR_01/radar_a/radar_a.0.ino
@@ -0,0 +1,25 @@
+
+#include <Servo.h>     
+
+#define SERVO_PWM_PIN 9
+
+Servo myservo;         
+
+/*----------------------------------------------------------------------
+  setup
+   Se ejecuta una sola vez al principio del programa. O cuando el arduino
+   se resetea.
+ ----------------------------------------------------------------------*/
+void setup() {
+
+  
+}
+
+
+/*----------------------------------------------------------------------
+  loop
+   Se ejecuta siempre, hasta el fin de los tiempos :-)
+ ----------------------------------------------------------------------*/
+void loop() {
+}
+ 
